cd3009e067f477e13443844b86c88dc3ac0dda13
TunedayCodes <fadipetunde5@gmail.com>
2024-03-05 06:46:26 +0100

    New commit for signup login system

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser= "root";
+$dbpass = "";
+$dbname = "login_sample_db";
+
+if(!$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
+   die("Connection error");
+}
+
+
+?>
